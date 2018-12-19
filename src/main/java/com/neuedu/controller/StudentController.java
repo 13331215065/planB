a9ab7cd2f42e6c6d9483393114ef142f65f6797d
menglw <menglw@yonyou.com>
2018-12-19 11:26:07 +0800

    -提交

--- src/main/java/com/neuedu/controller/StudentController.java
+++ src/main/java/com/neuedu/controller/StudentController.java
@@ -1,10 +1,15 @@
 package com.neuedu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.neuedu.service.IstudentService;
 
 @Controller
@@ -15,5 +20,14 @@ public class StudentController {
 	public String index() {
 		System.out.println(studentService.getStudents().toString());
 		return "index";
+	}
+	
+	@RequestMapping("/getData.do")
+	@ResponseBody
+	public List getData(String name) {
+		System.out.println(name);
+		List<String> list=new ArrayList<>();
+		list.add(name);
+		return list;
 	}
 }
--- target/SpringIOC-0.0.1-SNAPSHOT/WEB-INF/pages/index.jsp
+++ target/SpringIOC-0.0.1-SNAPSHOT/WEB-INF/pages/index.jsp
@@ -4,9 +4,27 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.min.js"></script>
 <title>Insert title here</title>
 </head>
 <body>
 	成功了！
 </body>
+<script type="text/javascript">
+//获取要显示的所有属性
+var shopcarts;
+	$.ajax({
+		url:"http://localhost:8080/SSM/getData.do",
+		type:"get",
+		dataType:"json",
+		data:{
+			name:"zhangsan"
+		},
+		async:false,
+		success:function(data){
+			shopcarts=data;
+		}
+	});
+console.log(shopcarts);
+</script>
 </html>
